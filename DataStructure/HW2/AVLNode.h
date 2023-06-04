04ddc8569a5cda93a476a6cf2e0ef33cbea54007
mkaynakci <73170422+mkaynakci@users.noreply.github.com>
2023-06-04 16:02:43 +0100

    Update AVLNode.h

---
+++ DataStructure/HW2/AVLNode.h
@@ -0,0 +1,32 @@
+#include <string>
+
+class AVLNode {
+private:
+    std::string chrom;
+    int pos;
+    std::string altBase;
+    AVLNode* left;
+    AVLNode* right;
+    int height;
+
+public:
+    AVLNode(std::string chrom, int pos, std::string altBase);
+    AVLNode* search(const std::string& chrom, int pos, const std::string& altBase);
+    AVLNode* remove(const std::string& chrom, int pos, const std::string& altBase);
+
+
+    std::string getChrom();
+    int getPos();
+    std::string getAltBase();
+    AVLNode* getLeft();
+    AVLNode* getRight();
+    int getHeight();
+
+    void setChrom(std::string chrom);
+    void setPos(int pos);
+    void setAltBase(std::string altBase);
+    void setLeft(AVLNode* left);
+    void setRight(AVLNode* right);
+    void setHeight(int height);
+};
+
